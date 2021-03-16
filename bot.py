feffcc05f9ce644340a64a486a99dd068c828d4b
ruthercode <ruthercode@gmail.com>
2021-03-16 13:42:14 +0300

    add /help command

---
+++ bot.py
@@ -0,0 +1,18 @@
+import telebot
+import os
+
+bot = telebot.TeleBot(os.environ["TOKEN"])
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, '/help - сводка о командах\n' +
+                          '/add - добавление разового напоминания напоминания\n'
+                          # "Ваше сообщение" "дата в формате часы.минуты/день.месяц.год" 
+                          "/add_repeat - добавление повторяющегося напоминания"
+                        ) 
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    pass
+        
+bot.polling(none_stop=True)
